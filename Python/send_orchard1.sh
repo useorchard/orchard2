c40ea25d03500fb95cb1c3098fb9efc2159f02f5
Orchard <orchard2@Orchard.(none)>
2015-11-03 19:16:42 -0400

    November 3, 2015

--- Python/send_orchard1.sh
+++ Python/send_orchard1.sh
@@ -1,1 +1,1 @@
-ssh orchard1@10.0.1.121 'bash ~/Orchard/Scripts/send.sh'
+ssh orchard1@10.0.1.121 'bash ~/Orchard/Scripts/send_log.sh; bash ~/Orchard/Scripts/send_scp.sh'
--- WebService/PHP/Cart_0/read_barcode4.php
+++ WebService/PHP/Cart_0/read_barcode4.php
@@ -1,23 +1,32 @@
 <?php
 
-
-// SETUP FOR SCRIPT
+// SETUP FOR SCRIPT:
 
 error_reporting(0);
 include("db_config.php");
 
+//TIMESTAMP 1
+//$timestamp1 = microtime (true);
+//mysql_query("REPLACE INTO Timing VALUES('1', 'Scan', '$timestamp1')");
+//
+
+$check = 0;
 $count = 0;
-$test = 1;
+$marker_counter = 4;
 
 
-// MARK LOCATION ON REAL-TIME CART WEIGHT DATABASE
+// MARK LOCATION ON REAL-TIME CART WEIGHT DATABASE:
 
 $marker_query = mysql_query("SELECT Id FROM Weight ORDER BY Id DESC LIMIT 1");
+//TIMESTAMP 2
+//$timestamp2 = microtime (true);
+//mysql_query("REPLACE INTO Timing VALUES('2', 'MinIdQuery', '$timestamp2')");
+//
 $marker_row = mysql_fetch_row($marker_query);
 $marker = $marker_row[0];
 
 
-// GET EXPECTED WEIGHT OF ITEM
+// GET EXPECTED WEIGHT OF ITEM:
 
 $response = array();
 
@@ -28,7 +37,10 @@ if( !(empty($_POST['barcode'])))
   $barcode=$_POST['barcode'];
 
   $result = mysql_query("SELECT Weight FROM Items WHERE (Barcode='$barcode')");
-
+  //TIMESTAMP 3
+  //$timestamp3 = microtime (true);
+  //mysql_query("REPLACE INTO Timing VALUES('3', 'ExpWeightQuery', '$timestamp3')");
+  //
   if($result>=0)
 
   {
@@ -51,23 +63,32 @@ if( !(empty($_POST['barcode'])))
   $data = $row[0];
 
 
-  // CHECK CART WEIGHT DATABASE FOR EXPECTED WEIGHT
+  // CHECK CART WEIGHT DATABASE FOR EXPECTED WEIGHT:
 
-  while ($count < 60)
+  while ($check == 0)
 
   {
 
-    mysql_query("INSERT INTO myorder VALUES('$count','evan')");
-    $verify_query = mysql_query("SELECT Delta FROM Weight WHERE Id >= '$marker' AND ABS(Delta-'$data')<=20");
+    $verify_query = mysql_query("SELECT Delta FROM Weight WHERE Id >= '$marker' AND ABS(Delta-'$data')<=60");
+    //TIMESTAMP 4+x
+    //$timestampx = microtime (true);
+    //mysql_query("REPLACE INTO Timing VALUES('$marker_counter', 'VerifyAttempt', '$timestampx')");
+    //
     $verify_row = mysql_fetch_row($verify_query);
     $real_weight = $verify_row[0];
 
-    if (!(empty($data)))
+    if (ABS($real_weight - $data)<=60)
 
     {
 
-      mysql_query("INSERT INTO myorder VALUES(2,'coulstring')");
+      // DETERMINE HOW TO NUMBER THIS VERIFIED COLUMN:
+
       $scan_query = mysql_query("SELECT Scan FROM Verified ORDER BY Scan DESC LIMIT 1;");
+      //TIMESTAMP x+1
+      //$marker_counter++;
+      //$timestampx1 = microtime (true);
+      //mysql_query("REPLACE INTO Timing VALUES('$marker_counter', 'NameIdQuery', '$timestampx1')");
+      //
       $scan_row = mysql_fetch_row($scan_query);
       $scan = $scan_row[0];
 
@@ -76,7 +97,6 @@ if( !(empty($_POST['barcode'])))
       {
 
         $scan_number = 1;
-        mysql_query("INSERT INTO myorder VALUES(21,'captain')");
 
       }
 
@@ -84,46 +104,65 @@ if( !(empty($_POST['barcode'])))
 
       {
 
-        $scan_number = $scan + 1;
-        mysql_query("INSERT INTO myorder VALUES(22,'leader')");
+        $scan_number = $scan++;
 
       }
 
+
+      // DETERMINE ITEM NAME:
       $name_query = mysql_query("SELECT Name FROM Items WHERE Barcode = '$barcode'");
+      //TIMESTAMP x+2
+      //$marker_counter++;
+      //$timestampx2 = microtime (true);
+      //mysql_query("REPLACE INTO Timing VALUES('$marker_counter', 'NameNameQuery', '$timestampx2')");
+      //
       $name_row = mysql_fetch_row($name_query);
       $name = $name_row[0];
-      $weight_query = mysql_query("SELECT Weight FROM Items WHERE Barcode = '$barcode'");
-      $weight_row = mysql_fetch_row($weight_query);
-      $exp_weight = $weight_row[0];
 
-      if($test = 1)
 
-      {
+      // ENTER VERIFIED INFORMATION:
 
-        mysql_query("INSERT INTO test VALUES('START')");
-        mysql_query("INSERT INTO test VALUES('$scan_number')");
-        mysql_query("INSERT INTO test VALUES('$name')");
-        mysql_query("INSERT INTO test VALUES('$barcode')");
-        mysql_query("INSERT INTO test VALUES('$exp_weight')");
-        mysql_query("INSERT INTO test VALUES('$real_weight')");
-        mysql_query("INSERT INTO test VALUES('$marker')");
-        mysql_query("INSERT INTO test VALUES('STOP')");
-        mysql_query("INSERT INTO Verified VALUES('$scan_number','$name','$barcode','$exp_weight','$real_weight'");
-        $test = 0;
+      $final_verify = mysql_query("REPLACE INTO Verified VALUES('$scan_number','$barcode','$data','$real_weight')");
+      //TIMESTAMP x+3
+      //$marker_counter++;
+      //$timestampx3 = microtime (true);
+      //mysql_query("REPLACE INTO Timing VALUES('$marker_counter', 'VerifyInsert', '$timestampx3')");
+      //
 
-      }
 
-      $count = 11;
+
+      // EXIT WHILE LOOP:
+
+      $check = 1;
+      $success = mysql_query("INSERT INTO test VALUES('SUCCESS')");
+      //TIMESTAMP x+4
+      //$marker_counter++;
+      //$timestampx4 = microtime (true);
+      //mysql_query("REPLACE INTO Timing VALUES('$marker_counter', 'SuccessInsert', '$timestampx4')");
+      //
 
     }
 
-    else
+
+    // HOW TO GET OUT OF WHILE LOOP
+
+    $count++;
+
+    if ($count == 30)
 
     {
 
-      $count = $count + 1;
+      $check = 1;
+      $timeout = mysql_query("INSERT INTO test VALUES('TIMEOUT')");
+      //TIMESTAMP x+1
+      //$marker_counter++;
+      //$timestampx5 = microtime (true);
+      //mysql_query("REPLACE INTO Timing VALUES('$marker_counter', 'TimeoutInsert', '$timestampx5')");
+      //
 
     }
+
+    $marker_counter++;
 
     sleep(1);
 
